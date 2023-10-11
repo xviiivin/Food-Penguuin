4d6c4b102469cb58c6eb151c258422c374016e7e
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-10-11 21:34:43 +0700

    admin: done with out nav

--- frontend/screens/Admin/HomeScreen.jsx
+++ frontend/screens/Admin/HomeScreen.jsx
@@ -1,11 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import React from 'react';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    Image,
+    Button,
+    TouchableOpacity, Pressable
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import RestaurantData from '../../data/RestaurantData.json'
 const HomeScreen = () => {
+    const header = 'คำขอเปิดร้านอาหาร'
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
+        <ScrollView>
+            <View className="p-10 bg-white w-full flex-1 gap-4">
+                {RestaurantData.map((value, index) => (
+                    <View className="border border-[#F6D544] rounded-md p-4">
+                        <View className=''>
+                            <Text className='font-notob text-lg underline'>{value.name}</Text>
+                            <View>
+                                <View className='mb-4'>
+                                    <Text>ที่ตั้ง: {value.food_court}</Text>
+                                    <Text>หมวดหมู่: {value.type}</Text>
+                                    <Text>เบอร์: {value.phone}</Text>
+                                </View>
+                                <View className='flex flex-row w-full items-center justify-center gap-4'>
+                                    <Pressable onPress={() => {
+                                        navigation.navigate("");
+                                    }} className='border border-[#F3F3F3] w-1/3 bg-[#F3F3F3] px-4 py-2 rounded-lg justify-center items-center '>
+                                        <Text className='font-notob'>ยกเลิก</Text>
+                                    </Pressable>
+                                    <Pressable className='border border-[#F6D544] w-1/3 bg-[#F6D544] px-4 py-2 rounded-lg justify-center items-center'>
+                                        <Text className='font-notob'>ยืนยัน</Text>
+                                    </Pressable>
+                                </View>
+                            </View>
+                        </View>
+
+
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
     )
 }
 
--- frontend/screens/Admin/SettingScreen.jsx
+++ frontend/screens/Admin/SettingScreen.jsx
@@ -1,14 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    Image,
+    Button,
+    TouchableOpacity, Pressable
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 const SettingScreen = () => {
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>SettingScreen</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <SafeAreaView>
+                <View className="flex flex-row justify-center items-center gap-4">
+                    <Image style={styles.profileImage} className='' source={{ uri: 'https://img.freepik.com/free-vector/big-win-surprise-banner-comic-style_1017-17792.jpg' }} />
+                    <View className='gap-y-1 w-1/2'>
+                        <Text className='font-notob text-lg'>วิวรรธน์ เหลียงกอบกิจ</Text>
+                        <Text className='font-notom color-[#A6A6A6] text-md'>64070232@kmitl.ac.th</Text>
+                    </View>
+                </View>
+                <TouchableOpacity style={{ margin: 30 }} onPress={() => { }}>
+                    <View style={styles.dis1}>
+                        <View style={styles.dis2}>
+                            <AntDesign name="logout" size={20} color="#B11E1E" />
+                            <Text style={{ marginLeft: 10 }}>ออกจากระบบ</Text>
+                        </View>
+                        <AntDesign name="right" size={20} color="black" />
+                    </View>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </ScrollView>
     )
 }
 
 export default SettingScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 20,
+    },
+    dis: {
+        flexDirection: 'row',
+        marginTop: 30,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    dis1: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    dis2: {
+        flexDirection: 'row',
+    },
+    logo: {
+        alignSelf: 'center',
+        marginLeft: 100,
+    },
+    profileImage:
+        { width: 100, height: 100, backgroundColor: "#ED8085", borderRadius: 50, marginBottom: 10, },
+});
