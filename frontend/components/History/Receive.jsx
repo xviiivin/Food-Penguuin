ad6662ed925beb63e5a91d2daa2758567ca06d73
xviiivin <88417506+xviiivin@users.noreply.github.com>
2023-10-18 13:37:28 +0700

    fix : Receive

--- frontend/components/History/Receive.jsx
+++ frontend/components/History/Receive.jsx
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
+import React from 'react';
 
 const Receive = () => {
-    return (
-        <View>
-            <Text>Receive</Text>
+  return (
+    <ScrollView className="p-10 bg-white">
+      <View className="flex gap-6">
+        <View className="flex flex-row justify-between items-center">
+          <View>
+            <Text className="font-notom text-[20px]">ร้านพี่ช้าง</Text>
+            <Text className="font-notom">16 Sep 2023 8.19 PM</Text>
+          </View>
+          <View>
+            <Text className="font-notoe bg-[#F6D544] p-2 rounded-xl">
+              ระยะเวลา ~ 60 นาที
+            </Text>
+          </View>
         </View>
-    )
-}
 
-export default Receive
+        <View className="flex flex-row justify-between items-center">
+          <Text className="font-notom  text-[18px]">สถานะ</Text>
+          <Text className="font-notom  text-[18px]">กำลังรอ</Text>
+        </View>
 
-const styles = StyleSheet.create({})
+        <View className="flex flex-row justify-between items-center">
+          <Text className="font-notom  text-[18px]">จำนวนคิวก่อนหน้า</Text>
+          <Text className="font-notom  text-[18px]">20 คิว</Text>
+        </View>
+        <View>
+          <View>
+            <Text className="font-notom  text-[18px]">รายการที่สั่ง</Text>
+          </View>
+
+          {/* mapตรงนี้ */}
+          <View className="border-b-[1px] border-[#D9D9D9] mb-2">
+            <TouchableOpacity style={styles.gridItem1}>
+              <View style={styles.picCover1}>
+                <Image
+                  className="w-full h-[150px]"
+                  source={{
+                    uri: 'https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
+                  }}
+                />
+              </View>
+              <View className="">
+                <View className="ml-3 space-y-1 mt-3">
+                  <Text className="font-notoe text-[15px]">เบอร์เกอร์</Text>
+                  <Text className="font-notom color-[#A3A3A3]">commend</Text>
+                </View>
+              </View>
+              <View className="top-0 right-0 mt-1 absolute mb-5 ">
+                <Text className="font-notom mt-2">2000 ฿</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className="flex flex-row justify-between">
+          <Text className="font-notom text-[18px]">ราคารวม</Text>
+          <Text className="font-notom text-[18px]">getราคารวม ฿ </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Receive;
+
+const styles = StyleSheet.create({
+  gridItem1: {
+    width: '100%',
+    marginVertical: 5,
+    height: 110,
+    overflow: 'hidden',
+    flexDirection: 'row',
+  },
+  picCover1: {
+    width: 92,
+    height: 92,
+    marginTop: 0,
+    marginBottom: 15,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+});
--- frontend/screens/Users/HistoryScreen.jsx
+++ frontend/screens/Users/HistoryScreen.jsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Receive from '../../components/History/Receive';
 import Order from '../../components/History/Order';
@@ -10,25 +10,29 @@ const HistoryScreen = () => {
   return (
     <>
       <Tab.Navigator>
-        <Tab.Screen name="Receive" component={Receive} options={{
-          tabBarLabel: () => (
-            <Text style={[{ color: "#202020" }]}>
-              ที่ต้องได้รับ
-            </Text>
-          ),
-
-        }} />
-        <Tab.Screen name="Order" component={Order} options={{
-          tabBarLabel: () => (
-            <Text style={[{ color: "#202020" }]}>
-              สั่งซื้อสำเร็จ
-            </Text>
-          ),
-
-        }} />
+        <Tab.Screen
+          name="Receive"
+          component={Receive}
+          options={{
+            tabBarIndicatorStyle: { backgroundColor: '#F6D33C' },
+            tabBarLabel: () => (
+              <Text style={[{ color: '#202020' }]}>ที่ต้องได้รับ</Text>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Order"
+          component={Order}
+          options={{
+            tabBarIndicatorStyle: { backgroundColor: '#F6D33C' },
+            tabBarLabel: () => (
+              <Text style={[{ color: '#202020' }]}>สั่งซื้อสำเร็จ</Text>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </>
-  )
-}
+  );
+};
 
-export default HistoryScreen
+export default HistoryScreen;
